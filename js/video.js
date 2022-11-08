37f5bcedfd8759a6d0c4fe71c0480ac35454a2a8
Weiskoja <weiskoja@umich.edu>
2022-11-07 21:00:05 -0500

    DOM selectors fixed and finished play,pause,old,original,speed up and slow down

--- js/video.js
+++ js/video.js
@@ -11,41 +11,57 @@ var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
-	console.log(typeof(video));
-	console.log(video);
 });
 
 
 // play video //
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play()
 });
 
 
 // pause video
-//document.querySelector("#pause").addEventListener("click", function() {
-//	video.pause()
-//	console.log("Pause Video");
-//});
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause()
+	console.log("Pause Video");
+});
 
 
 // slow down video
-//document.querySelector("#slower").addEventListener("click", function() {
-//	video.playbackrate = curRate - 0.5
-//	console.log("Pause Video");
-//});
+document.querySelector("#slower").addEventListener("click", function() { 
+	video.playbackRate *=  0.9;
+	console.log(video.playbackRate);
+	console.log( "Current speed is: " + String((video.playbackRate.toFixed(2) * 100 ))  + "%")
+	//console.log("video speed: " + String( (video.playbackrate  * 100) ) + "%");
+});
 
 // speed up video
-//document.querySelector("#faster").addEventListener("click", function() {
-//	video.playbackrate = curRate + 0.5
-//	console.log("Pause Video");
-//});
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate *= 1.1; 
+	console.log(video.playbackRate);
+	console.log( "Current speed is: " + String((video.playbackRate.toFixed(2) * 100 ))  + "%")
+	//console.log("video speed: " + String( (video.playbackrate  * 100) ) + "%");
+});
 
 // skip video
 //document.querySelector("#pause").addEventListener("click", function() {
 //	video.e = curRate - 0.5
 //	console.log("Pause Video");
 //});
+
+// oldstyle video
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.className = "oldSchool"
+	console.log(video.className)
+})
+
+// original style the video
+document.querySelector("#orig").addEventListener("click", function() {
+	video.className = "video"
+	console.log(video.className)
+	console.log("this")
+})
 
 
 
